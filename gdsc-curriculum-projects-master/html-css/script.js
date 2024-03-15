ad6b15156b84ee79a970e23ab053022e816f2bae
HaiikallR <haikalsitafajriramadhan@gmail.com>
2024-03-15 15:36:22 +0700

    first commit

---
+++ gdsc-curriculum-projects-master/html-css/script.js
@@ -0,0 +1,4 @@
+const button = document.getElementById('button')
+//buatkan variabel bernama button ngambil document dari id yang bernama button data diambil dari html
+
+button.addEventListener('Click')
